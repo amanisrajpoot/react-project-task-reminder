6c4f594383681f4544135303c2722db470d1924f
Aman Rajpoot <amanrajpoot59@gmail.com>
2021-06-15 14:17:23 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="about">
             <h4>Version 1.0.0</h4>
-            <Link to='/react-project-task-reminder'>Go Back</Link>
+            <Link to='/'>Go Back</Link>
         </div>
     )
 }
