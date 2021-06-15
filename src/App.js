42d7f8c73601988681c163a448aa969c92d8f14a
Aman Rajpoot <amanrajpoot59@gmail.com>
2021-06-15 13:05:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
-import Button from "./components/Button"
 import Footer from "./components/Footer"
 import About from "./components/About"
 
